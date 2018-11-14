6e8d43db13b0dadbf828f784486259efde42944c
DT-1236 <WEMascarinas@gmail.com>
2018-11-14 14:13:58 -0800

    hack-or-snooze initialized

---
+++ api-classes.js
@@ -0,0 +1,12 @@
+const BASE_URL = "https://hack-or-snooze-v2.herokuapp.com";
+
+class StoryList {
+  static getStories(cb) {
+    // fetch stories from API
+    return cb(/* fetched stories go here */);
+  }
+}
+
+class User {}
+
+class Story {}
